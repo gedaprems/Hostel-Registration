dc9665b4a6cae33c4ba36aba0a5ff83172305cfc
Premsagar Gedam <gedamprems@gmail.com>
2023-12-25 23:02:25 +0530

    Added Session functionality in the login section

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid');
 const mongoose = require('mongoose');
+const fs = require('fs');
 const bodyTOObj = require('./scripts/jsobj.js');
 const insertNewUser = require('./mongodbinsertdata.js');
 const isValidUser = require('./utils/isValidUser.js');
+const exp = require('constants');
 
 const app = express();
 
@@ -12,6 +16,8 @@ app.set('view engine','ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}));
 
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Model creation -----------------
 
@@ -33,6 +39,37 @@ app.get('/',function(req,res){
     res.render('index',{msg:msg,title:"Home | Hostel Registration"});
 })
 
+app.get('/login', (req,res)=>{
+
+    const sessionToken = req.cookies['session_token'];
+    if(!sessionToken){
+        msg="Please Login First!";
+        res.redirect('/');
+    }
+
+    // need to add user is still present in the data base  or not! 
+
+    fs.readFile('./session/session.json','utf8',(err,data)=>{
+        console.log(data);
+        const obj = JSON.parse(data);
+
+        if(obj[sessionToken]){
+            if(obj.expiresAt < new Date()){
+                msg="Session got Expired!";
+                res.redirect('/');
+            }
+            res.render('login',{msg:"Sucessully Logged In and Session is stored!",title:"Logged In"});
+        }else{
+            msg="Session got Expired!";
+            res.redirect('/');
+        }
+    })
+
+    
+    
+    
+})
+
 app.post('/login', (req,res)=>{
     const userid = req.body.userid;
     const password = req.body.password;
@@ -41,7 +78,42 @@ app.post('/login', (req,res)=>{
         
         if( output == true){
             msg = "";
-            res.render('login',{msg:"Sucessully Logged In!",title:"Logged In"});
+
+            // Checking session is already present 
+
+            const sessionTokenOld = req.cookies['session_token'];
+
+            if(sessionTokenOld){
+                // need to add user still present in data base or not / they updated the password and username or etc or the session is with same user or not
+                res.render('login',{msg:"Sucessully Logged In and Session is stored!",title:"Logged In"});
+            }
+            // Session Creation
+            const sessionToken = uuid.v4();
+            const expiresAt =  new Date().setMonth(new Date().getMonth()+1);
+
+            const currUserSession = {
+                expiresAt,
+                userId : userid,
+            }
+
+            fs.readFile('./session/session.json','utf8',(err,data)=>{
+                console.log(data);
+                let obj = JSON.parse(data);
+                obj[sessionToken] = currUserSession;
+                let objJson = JSON.stringify(obj);
+                console.log(objJson);
+                fs.writeFile('./session/session.json',objJson,(err)=>{
+                    if(err){
+                        msg = "Error while login to store session";
+                        res.redirect("/");
+                    }
+                    res.cookie("session_token", sessionToken, {maxAge: expiresAt});
+                    res.render('login',{msg:"Sucessully Logged In ans Session is stored!",title:"Logged In"});
+                })
+            })
+
+
+            
         }else if(output == false){
             msg = "Invalid Password !";
             res.redirect('/');
---
+++ practice.js
@@ -0,0 +1,6 @@
+const thisMonth = new Date().setMonth(new Date().getMonth());
+console.log(thisMonth);
+
+const nextMonth = new Date().setMonth(new Date().getMonth()+1)
+
+console.log(nextMonth);
